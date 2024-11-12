0e943ec365e5183573c0b797fd572a390f46f375
Djali Suandre <djalisuandre_2226240011@mhs.mdp.ac.id>
2024-11-12 13:49:19 +0700

    updates

--- app_server/controllers/fakultasController.js
+++ app_server/controllers/fakultasController.js
@@ -1,23 +1,21 @@
 const axios = require("axios");
 
 const index = async (req, res) => {
-    try {
-        // mendapatkan data fakultas dari API eksternal
-        const response = await axios.get(
-            "http://localhost:3000/api/fakultas"
-        );
+  try {
+    // mendapatkan data fakultas dari API eksternal
+    const response = await axios.get("http://localhost:3000/api/fakultas");
 
-        const fakultas = response.data;
+    const fakultas = response.data;
 
-        res.render("fakultas", {
-            title: "Halaman Fakultas",
-            fakultas,
-            layout: "main",
-        });
-    }catch (error)  {
-        console.error(error.mesage);
-        res.status(500),send("Gagal mendapatkan data fakultas dari api");
-    }
+    res.render("fakultas", {
+      title: "Halaman Fakultas",
+      fakultas,
+      layout: "main",
+    });
+  } catch (error) {
+    console.error(error.mesage);
+    res.status(500), send("Gagal mendapatkan data fakultas dari api");
+  }
 };
 
-module.exports = {index};
+module.exports = { index };
--- app_server/controllers/prodiController.js
+++ app_server/controllers/prodiController.js
@@ -1,13 +1,21 @@
-const prodi = (req, res) => {
-    const programstudi =[
-        {NamaProdi: "Sistem Informasi", Fakultas: "FIKR", Singkatan: "SI"},
-        {NamaProdi: "Informatika", Fakultas: "FIKR", Singkatan: "IF"},
-        {NamaProdi: "Teknik Elektro", Fakultas: "FIKR", Singkatan: "TE"},
-        {NamaProdi: "Manajemen Informatika", Fakultas: "FIKR",Singkatan: "MI"},
-        {NamaProdi: "Manajemen", Fakultas: "FEB", Singkatan: "MJ"},
-        {NamaProdi: "Akuntasi", Fakultas: "FEB", Singkatan: "AK"}
-    ];
-    res.render('prodi',{title: 'halaman prodi',programstudi,layout:'main'});
+const axios = require("axios");
+
+const index = async (req, res) => {
+  try {
+    // mendapatkan data prodi dari API eksternal
+    const response = await axios.get("http://localhost:3000/api/prodi");
+
+    const prodi = response.data;
+
+    res.render("prodi", {
+      title: "Halaman Prodi",
+      prodi,
+      layout: "main",
+    });
+  } catch (error) {
+    console.error(error.mesage);
+    res.status(500), send("Gagal mendapatkan data prodi dari api");
+  }
 };
 
-module.exports = {prodi}
+module.exports = { index };
