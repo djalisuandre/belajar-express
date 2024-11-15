3f7b990aa769380e5733d50a152845691ac47d89
Djali Suandre <djalisuandre_2226240011@mhs.mdp.ac.id>
2024-11-15 11:04:12 +0700

    Update authController.js

--- app_api/controllers/authController.js
+++ app_api/controllers/authController.js
@@ -41,4 +41,40 @@ exports.register = async (req, res) => {
 };
 
 // fungsi login
-exports.login = async (req, res) => {};
+exports.login = async (req, res) => {
+  // Ambil data dari body
+  const { email, password } = req.body;
+  try {
+    // Cek apakah email belum terdaftar
+    let user = await User.findOne({ email });
+    if (!user) {
+      // Jika email belum terdaftar, kirim respon error
+      return res.status(400).json({
+        message: "Email or password does not exist",
+      });
+    }
+    // Jika email terdaftar
+    // Cek apakah password cocok
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      // Jika password tidak cocok, kirim respon error
+      return res.status(400).json({
+        message: "Email or password does not exist",
+      });
+    }
+    // Jika email dan password cocok
+    // proses buat token jwt
+    const payload = {
+      id: user.id,
+      role: user.role,
+    };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h", // token kadaluarsa dalam 1 jam
+    });
+    res.json({ token }); // kirim token ke client sebagai respon
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
