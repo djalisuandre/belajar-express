41dcbbdaf9e0c62481b01e57d953e4d9c7916c71
Djali Suandre <djalisuandre_2226240011@mhs.mdp.ac.id>
2024-11-15 10:16:50 +0700

    Update user.js

--- app_api/models/user.js
+++ app_api/models/user.js
@@ -1,7 +1,6 @@
 // Mengimpor modul mongoose
-const e = require("express");
 const mongoose = require("mongoose");
-const { create } = require("./prodi");
+const bcrypt = require("bcrypt");
 
 // Definisikan skema untuk fakultas
 const userSchema = new mongoose.Schema({
@@ -27,6 +26,16 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  // Jika password tidak diubah, lanjutkan tanpa enkripsi
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt); // enkripsi password
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
